4b9e7b6ecbf35b1ecd2defe6590164fc56d85c0b
Joe <hover2pi@gmail.com>
2014-02-11 21:15:54 -0500

    Removed Joe's path from dependencies.

--- BDdb.py
+++ BDdb.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # BDNYC database
 import io, os, sqlite3 as sql, numpy as np, matplotlib.pyplot as plt, astropy.io.fits as pf, utilities as u, astrotools as a
-path = '/Users/Joe/Documents/Python/'
 
 class get_db:
   def __init__(self, dbpath):
