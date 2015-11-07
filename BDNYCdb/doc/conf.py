81711f2ddebe22a07d270b74242070b0a4b36635
Joe Filippazzo <hover2pi@gmail.com>
2015-11-06 23:35:06 -0500

    Update conf.py

--- BDNYCdb/doc/conf.py
+++ BDNYCdb/doc/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -112,7 +113,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
