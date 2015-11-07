47d373f6d7bce172791337022350cd79827473b9
Joe Filippazzo <hover2pi@gmail.com>
2015-11-07 00:00:52 -0500

    Update conf.py

--- BDNYCdb/doc/conf.py
+++ BDNYCdb/doc/conf.py
@@ -33,7 +33,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'napoleon',
+    'sphinx.ext.autodoc', 
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
